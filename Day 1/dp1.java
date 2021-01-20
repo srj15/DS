4b5c5aaadc47bb8b8b540ae4f9f38db2d745cba5
Sooraj <gsooraj15@gmail.com>
2021-01-20 16:01:11 +0530

    Optimized Code

--- Day 1/dp1.java
+++ Day 1/dp1.java
@@ -1,35 +1,26 @@
-class Solution {
-    final int inf = 1ef;
-    int [][] dp,vis;
+// Time Complexity: O(N^2), where N is the length of A.
 
+// Space Complexity: O(1) in additional space complexity.
+
+class Solution {
     public int minFallingPathSum(int[][] A) {
-        int col = A[0].length;
+        int N = A.length;
+        for(int r=N-2;r>=0;r--){
+            for(int c=0;c<N;c++){
+                int best = A[r+1][c];
+                if(c>0){
+                    best = Math.min(best, A[r+1][c-1]);
+                }
+                if(c+1<N){
+                    best = Math.min(best, A[r+1][c+1]);
+                }
+                A[r][c]+=best;
+            }
+        }
         int ans = Integer.MAX_VALUE;
-        for(int i=0 ; i<col ; i++){
-            ans = Math.min(ans, minSum(0,i,A));
+        for(int i : A[0]){
+            ans = Math.min(ans, i);
         }
         return ans;
     }
-
-    int minSum(int x, int y, int[][] A){
-        int ans = A[x][y];
-
-        if(x == A.length)
-            return 0;
-        if(y == inf || y == -1)
-            return 0;
-
-        if(vis[x][y])
-            return ans;
-
-        ans = Math.min(ans, A[x][y] + (x+1,y,A));
-
-        ans = Math.min(ans, A[x][y] + (x+1,y+1,A));
-
-        ans = Math.min(ans, A[x][y] + (x+1,y-1,A));
-
-        vis[x][y] = 1;
-        return ans;
-        
-    } 
 }
